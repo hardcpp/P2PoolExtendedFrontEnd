7ba4c832619651615397b05a413e7faa6cfd324f
blixnood <blixnood-github.com@lurkmore.com>
2013-12-26 02:12:05 -0500

    Final changes--maybe.

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ function LoadData() {
       tr.append('td').text(function(block){return block.number});
       tr.append('td').text(function(block){return new Date(1000*block.ts).toString()});
       tr.append('td').append('a').text(function(block){return block.hash}).attr('href', function(block){return currency_info.block_explorer_url_prefix + block.hash});
-      tr.append('td').append('a').text('->').attr('href', function(block){return 'share.html#' + block.share});
+      tr.append('td').append('a').text('>').attr('href', function(block){return 'share.html#' + block.share});
     });
   });
  
